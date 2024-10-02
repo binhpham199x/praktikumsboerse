fcfd72c823b7f24fa10580f35ba89403da3bb871
Binh Pham <binh.pham@synaigy.com>
2024-10-02 17:20:53 +0200

    feat: implement auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthLogInDto, AuthSignUpDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthSignUpDto) {
+    return this.authService.signup(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: AuthLogInDto) {
+    return this.authService.login(dto);
+  }
+}
