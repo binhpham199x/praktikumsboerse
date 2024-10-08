4f371101f65ecc402af95a2bbee546fe01eef38e
Binh Pham <binh.pham@synaigy.com>
2024-10-08 14:09:31 +0200

    feat: add simple logic for get request in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Logger, Request, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtGuard } from '../auth/guard';
 
 @Controller('user')
-export class UserController {}
+@UseGuards(JwtGuard)
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('info')
+  getInfo(@Request() req) {
+    Logger.log(req);
+    Logger.log(req.user);
+    return req.user;
+  }
+}
