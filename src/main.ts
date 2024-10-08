70315374ccc434415c4a1e1d8b1e6a688fde9eea
Binh Pham <binh.pham@synaigy.com>
2024-10-08 11:30:45 +0200

    feat: add a log to  logger for application URL in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ZodValidationPipe } from '@anatine/zod-nestjs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  const port = 3000;
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port);
+  });
 }
 bootstrap();
