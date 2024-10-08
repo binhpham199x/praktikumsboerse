0415a36429ee181a68d9491132b288c66cacd8f2
Binh Pham <binh.pham@synaigy.com>
2024-10-08 11:42:23 +0200

    feat: adjust user schema according to prisma schema

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -7,6 +7,32 @@ export const userSchema = z.object({
   password: z.string(),
   role: RoleEnum,
 
+  companyName: z.string().optional(),
+  fristName: z.string().optional(),
+  lastName: z.string().optional(),
+  description: z.string().optional(),
+  city: z.string().optional(),
+
   createdAt: z.date().or(z.string()),
   updatedAt: z.date().or(z.string()),
 });
+
+// model User {
+//   userId    Int      @id @default(autoincrement())
+//   createdAt DateTime @default(now())
+//   updatedAt DateTime @updatedAt
+
+//   email String @unique
+//   hash  String
+
+//   role Role
+
+//   companyName String?
+//   firstName   String?
+//   lastName    String?
+//   description String?
+//   city        String?
+
+//   internship   Internship[]
+//   applications Application[]
+// }
