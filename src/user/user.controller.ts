aa1554918d7c2120d1a07ab5098cc648dfafcb44
Binh Pham <binh.pham@synaigy.com>
2024-10-11 10:26:39 +0200

    feat: add Api tags and Api bearer auth for swagger UI in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Logger, Request, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtGuard } from '../auth/guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
 @UseGuards(JwtGuard)
+@ApiBearerAuth('jwt')
+@ApiTags('User')
 export class UserController {
   constructor(private userService: UserService) {}
 
